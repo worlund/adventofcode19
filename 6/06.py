8d7f1368f218a2ca15d942928736b235e14d6351
worlund <worlund@kth.se>
2020-01-08 11:28:45 +0100

    better search for 06

--- 6/06.py
+++ 6/06.py
@@ -1,5 +1,6 @@
 import sys
 import queue
+from timeit import default_timer as timer
 
 def bfs(orbits, root):
 	q = queue.Queue()
@@ -16,7 +17,7 @@ def bfs(orbits, root):
 				visited[o[1]] = visited[o[0]]+1
 				paths[o[1]] = v
 				total+=visited[o[1]]
-	return total, paths
+	return total, paths, visited
 
 def dist(node1, node2, paths):
 	curr = node1
@@ -38,12 +39,34 @@ def dist(node1, node2, paths):
 			break
 	return n1steps+n2steps-2
 
+def dist_better(node1, node2, paths, weights):
+	curr = node1
+	target = node2
+	targetPath = set()
+	dist = 0
+
+	#get whole path for target node
+	while paths[target] != "COM":
+		targetPath.add(paths[target])
+		target = paths[target]
+
+	#find first occurence of intersection in target path for start node
+	while paths[curr] != "COM":
+		if paths[curr] in targetPath:
+			dist = weights[node1] + weights[node2] - 2*weights[paths[curr]] - 2
+			break
+		curr = paths[curr]
+	return dist
+
 orbits = [x.strip().split(")") for x in sys.stdin.readlines()]
 
 root = "COM"
 n1 = "YOU"
 n2 = "SAN"
 
-res, paths = bfs(orbits, root)
+res, paths, weights = bfs(orbits, root)
 print("total orbits: ",res)
-print("orbits between {} and {}: {}".format(n1, n2, dist("YOU", "SAN", paths)))
+res1 = dist("YOU", "SAN", paths)
+print("orbits between {} and {}: {}".format(n1, n2, res1))
+res2 = dist_better(n1,n2,paths, weights)
+print("orbits between {} and {}: {}".format(n1, n2, res2))
